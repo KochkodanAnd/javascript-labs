7f84443b6e7d44c14818433301c8ca4a960a972e
KochkodanAnd <145270892+KochkodanAnd@users.noreply.github.com>
2026-05-11 22:04:37 -0500

    Create SpeakHello.js

---
+++ lab3/SpeakHello.js
@@ -0,0 +1,11 @@
+(function(window){
+    var speakWord = "Hello";
+let helloSpeakWord={};
+
+helloSpeakWord.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.helloSpeakWord=helloSpeakWord;
+
+})(window);
